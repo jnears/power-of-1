e09ac04b1d7078c21d7a2d1e11f7fe0f5abaad26
Jonathan Nears <jnears@essex.ac.uk>
2013-04-22 14:55:34 +0100

    Ruby basic install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PowerOf1::Application.config.secret_token = '9cc4c6e5a76e330151baf121abb00aace059fbc172f8bbe711c11004f9e499563f814208b4e57c71f2a7d6b5ac6c9a91a59dec66248acd2e5d4a5c921bf23ead'

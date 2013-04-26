d7450364c5931286de7d0e8ed636354f5bce097c
Jonathan Nears <jnears@essex.ac.uk>
2013-04-26 12:16:53 +0100

    Fix: Remove the validates_exitence_of

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -5,9 +5,6 @@ class Runner < ActiveRecord::Base
     #NOTE - SINGULAR
     belongs_to :coach
 
-    #makes sure if coach has been deleted and runner is edited you cannot save it to a caoch that no longer exists
-    validates_existence_of :coach, :both => false
-
     validates :first_name, :last_name, :gender, :county, :club, :date_of_birth, :presence => true
 
 end
